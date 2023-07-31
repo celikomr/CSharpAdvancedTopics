16ef86027df387e7e871b7c8cd12aa6f31c58793
Ömer Çelik <oclk@outlook.com.tr>
2023-07-31 23:23:07 +0300

    feat: Developed Progam.cs with advanced samples in Generics

--- LanguageFeatures/Generics/Program.cs
+++ LanguageFeatures/Generics/Program.cs
@@ -1,2 +1,16 @@
 ﻿using Generics;
 
+// Covariance and Contravariance
+// AnimalFactory<IAnimal> animalFactory = AnimalFactoryImpl.CreateDog;
+// IAnimal animal = animalFactory();
+// animal.MakeSound(); // Outputs: Woof!
+
+// Generic Delegates and Events
+// Button button = new Button();
+// button.Clicked += GenericEventHandlerImp.Button_Clicked;
+// button.OnClicked(); // Outputs: Button clicked!
+
+// Generic Constraints with Enums
+Logger<LogLevel> logger = new Logger<LogLevel>();
+logger.Log(LogLevel.Warning, "This is a warning message.");
+logger.Log(LogLevel.Error, "This is an error message.");
