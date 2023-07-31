429ab82864df02253b43227ad6b0574be1f4aea3
Ömer Çelik <oclk@outlook.com.tr>
2023-07-31 22:56:26 +0300

    feat: Added Advanced Usage Section to the Workbench.cs in Generics

--- LanguageFeatures/Generics/Workbench.cs
+++ LanguageFeatures/Generics/Workbench.cs
@@ -1,60 +1,106 @@
 ﻿namespace Generics;
 
-// Type parameter T in angle brackets
-public class GenericList<T>
+
+#region Multiple Type Parameters
+public class Pair<T, U>
 {
-    // The nested class is also generic on T.
-    private class Node
+    public Pair(T first, U second)
     {
-        // T used in non-generic constructor.
-        public Node(T t)
-        {
-            next = null;
-            data = t;
-        }
-
-        private Node? next;
-        public Node? Next
-        {
-            get { return next; }
-            set { next = value; }
-        }
-
-        // T as private member data type.
-        private T data;
-
-        // T as return type of property.
-        public T Data
-        {
-            get { return data; }
-            set { data = value; }
-        }
+        First = first;
+        Second = second;
     }
 
-    private Node? head;
+    public T First { get; set; }
+    public U Second { get; set; }
+}
 
-    // Constructor
-    public GenericList()
+// The example above defines the generic class Pair for two different types.
+// The Pair class can be used to store two different types of values and return their first and second elements.
+#endregion
+
+#region Covariance and Contravariance
+public interface IAnimal
+{
+    void MakeSound();
+}
+
+public class Dog : IAnimal
+{
+    public void MakeSound()
     {
-        head = null;
-    }
-
-    // T as method parameter type:
-    public void AddHead(T t)
-    {
-        Node n = new (t);
-        n.Next = head;
-        head = n;
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        Node? current = head;
-
-        while (current != null)
-        {
-            yield return current.Data;
-            current = current.Next;
-        }
+        Console.WriteLine("Woof!");
     }
 }
+
+public delegate TAnimal AnimalFactory<out TAnimal>();
+
+public class AnimalFactoryImpl
+{
+    public static Dog CreateDog()
+    {
+        return new Dog();
+    }
+}
+#endregion
+
+#region Generic Delegates and Events
+public delegate void GenericEventHandler<TEventArgs>(object sender, TEventArgs e);
+
+public class Button
+{
+    public event GenericEventHandler<EventArgs> Clicked;
+
+    public void OnClicked()
+    {
+        Clicked?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+public class GenericEventHandlerImp
+{
+    public static void Button_Clicked(object sender, EventArgs e)
+    {
+        Console.WriteLine("Button clicked!");
+    }
+}
+
+// The example above demonstrates using a generic delegate, GenericEventHandler<TEventArgs>, and creating an event using this delegate.
+// The Button class defines the generic event Clicked and the OnClicked method is used to trigger this event.
+#endregion
+
+#region Recursive Generics
+public class TreeNode<T>
+{
+    public T Value { get; set; }
+    public List<TreeNode<T>> Children { get; set; }
+
+    public TreeNode(T value)
+    {
+        Value = value;
+        Children = new List<TreeNode<T>>();
+    }
+}
+
+// The above example defines the recursive generic class TreeNode<T> to create a tree structure.
+// Each node is represented by a Value of a certain type, and its Children properties are kept in a List<TreeNode<T>> type that represents child nodes.
+#endregion
+
+#region Generic Constraints with Enums
+public enum LogLevel
+{
+    Info,
+    Warning,
+    Error
+}
+
+public class Logger<TLogLevel> where TLogLevel : Enum
+{
+    public void Log(TLogLevel logLevel, string message)
+    {
+        Console.WriteLine($"[{logLevel}] {message}");
+    }
+}
+
+// The above example performs logging using the generic class Logger<TLogLevel>.
+// The TLogLevel type must be Enum so that restrictions are placed on only a specific enum type and valid log levels are limited to only a specific Enum type.
+#endregion
