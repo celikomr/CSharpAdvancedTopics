0ea63a7e2708a040b7978f0fdc9920a0b9872436
Ömer Çelik <oclk@outlook.com.tr>
2023-08-06 17:34:05 +0300

    feat: Added Anonymous Methods, Closure and Captured Variables and Built-in Delegate Types to the Delegates

--- LanguageFeatures/Delegates/Program.cs
+++ LanguageFeatures/Delegates/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿namespace Delegates;
+
+class Program
+{
+    // Anonymous Methods
+    delegate void GreetingDelegate(string message);
+
+    // Closure and Captured Variables
+    delegate void CounterDelegate();
+
+
+    static void Main(string[] args)
+    {
+        #region Anonymous Methods
+        GreetingDelegate greet = delegate (string msg)
+        {
+            Console.WriteLine("Hello, " + msg);
+        };
+
+        greet("John");
+        #endregion
+
+        #region Closure and Captured Variables
+        int count = 0;
+        CounterDelegate counter = () =>
+        {
+            count++;
+            Console.WriteLine("Count: " + count);
+        };
+
+        counter();
+        counter();
+        #endregion
+
+        #region Built-in Delegate Types
+        Predicate<int> isEven = num => num % 2 == 0;
+        Console.WriteLine(isEven(4)); // True
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        var evenNumbers = Array.FindAll(numbers, isEven);
+        Console.WriteLine(string.Join(", ", evenNumbers)); // 2, 4
+        #endregion
+    }
+}
