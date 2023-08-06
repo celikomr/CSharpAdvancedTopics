7687a6d59bf82f9426bdf8a5d54659c1f0876a0f
Ömer Çelik <oclk@outlook.com.tr>
2023-08-06 17:37:39 +0300

    feat: Added Custom Delegate Types and Events with Delegates to the Delegates

--- LanguageFeatures/Delegates/Program.cs
+++ LanguageFeatures/Delegates/Program.cs
@@ -8,6 +8,8 @@ class Program
     // Closure and Captured Variables
     delegate void CounterDelegate();
 
+    // Custom Delegate Types
+    delegate void CalculationDelegate(int a, int b);
 
     static void Main(string[] args)
     {
@@ -40,5 +42,43 @@ class Program
         var evenNumbers = Array.FindAll(numbers, isEven);
         Console.WriteLine(string.Join(", ", evenNumbers)); // 2, 4
         #endregion
+
+        #region Custom Delegate Types
+        CalculationDelegate add = (x, y) => Console.WriteLine("Sum: " + (x + y));
+        CalculationDelegate subtract = (x, y) => Console.WriteLine("Difference: " + (x - y));
+
+        add(10, 5);
+        subtract(20, 7);
+        #endregion
+
+        #region Events with Delegates
+        Publisher publisher = new Publisher();
+        Subscriber subscriber = new Subscriber();
+
+        publisher.EventOccurred += subscriber.HandleEvent;
+        publisher.DoSomething();
+        #endregion
     }
 }
+
+#region Events with Delegates
+class Publisher
+{
+    public delegate void EventHandler(string message);
+    public event EventHandler EventOccurred;
+
+    public void DoSomething()
+    {
+        Console.WriteLine("Something happened.");
+        EventOccurred?.Invoke("Event occurred!");
+    }
+}
+
+class Subscriber
+{
+    public void HandleEvent(string message)
+    {
+        Console.WriteLine("Subscriber received: " + message);
+    }
+}
+#endregion
